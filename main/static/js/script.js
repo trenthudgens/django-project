18fafcc1c9de53abbe876f4bd9ac666f410890f6
trenthudgens <hudgens.trent@gmail.com>
2023-06-16 15:10:10 -0600

    first commit

---
+++ main/static/js/script.js
@@ -0,0 +1,3 @@
+document.getElementById('changeContent').addEventListener('click', function() {
+    document.getElementById('content').textContent = 'You clicked the button, and I changed the content!';
+});
